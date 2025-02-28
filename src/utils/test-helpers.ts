f5922afb75453bf70d4527cfab387941949312fc
Lucas Pope <lpopedv@proton.me>
2025-02-28 19:12:24 -0300

    feat: create 'clearDatabase' test helper fn

---
+++ src/utils/test-helpers.ts
@@ -0,0 +1,24 @@
+import { prisma } from '~/lib'
+
+const clearDatabase = async (): Promise<void> => {
+	try {
+		const tablenames = await prisma.$queryRaw<Array<{ tablename: string }>>`
+    SELECT tablename FROM pg_tables WHERE schemaname = 'public'
+  `
+
+		const tables = tablenames
+			.map(({ tablename }) => tablename)
+			.filter((name) => name !== '_prisma_migrations')
+			.map((name) => `"public"."${name}"`)
+			.join(', ')
+
+		await prisma.$executeRawUnsafe(`TRUNCATE TABLE ${tables} CASCADE;`)
+		await prisma.$disconnect()
+	} catch (error) {
+		console.error({ error })
+	}
+}
+
+export const TestHelpers = {
+	clearDatabase,
+}
