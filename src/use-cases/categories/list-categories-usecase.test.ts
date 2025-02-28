116b3a900020e51ca835c93104ac5b7db2d91e50
Lucas Pope <lpopedv@proton.me>
2025-02-28 19:12:59 -0300

    feat: create 'listCategories' use case and test

---
+++ src/use-cases/categories/list-categories-usecase.test.ts
@@ -0,0 +1,28 @@
+import { afterAll, describe, it, expect } from 'vitest'
+import { Category } from '~/schemas/category'
+import { TestHelpers } from '~/utils/test-helpers'
+import { ListCategoriesUseCase } from './list-categories-usecase'
+import { CreateCategoryUseCase } from './create-category-usecase'
+
+afterAll(async () => await TestHelpers.clearDatabase())
+
+describe('handle', () => {
+	it('should be able to list all categories', async () => {
+		const category01: Category = {
+			title: 'New Category Test 01',
+			description: 'Category 01 for test use case',
+		}
+
+		const category02: Category = {
+			title: 'New Category Test 02',
+			description: 'Category 02 for test use case',
+		}
+
+		await CreateCategoryUseCase.handle(category01)
+		await CreateCategoryUseCase.handle(category02)
+
+		const categories = await ListCategoriesUseCase.handle()
+
+		expect(categories).toHaveLength(2)
+	})
+})
---
+++ src/use-cases/categories/list-categories-usecase.ts
@@ -0,0 +1,20 @@
+import { prisma } from '~/lib'
+import type { Category } from '~/schemas/category'
+
+const handle = async (): Promise<Category[]> => {
+	const databaseCategories = await prisma.category.findMany()
+
+	const parsedCategories = databaseCategories.map(
+		(category): Category => ({
+			id: category.id,
+			title: category.title,
+			description: category.description ?? undefined,
+		}),
+	)
+
+	return parsedCategories
+}
+
+export const ListCategoriesUseCase = {
+	handle,
+}
