fe7aa37250cce838fdc4e4592638dc88957525c5
Lucas Pope <lpopedv@proton.me>
2025-02-28 19:48:50 -0300

    feat: create 'updateCategory' use case and test

---
+++ src/use-cases/categories/update-category-usecase.test.ts
@@ -0,0 +1,42 @@
+import { afterAll, describe, it, expect } from 'vitest'
+import { Category } from '~/schemas/category'
+import { TestHelpers } from '~/utils/test-helpers'
+import { CreateCategoryUseCase } from './create-category-usecase'
+import { UpdateCategoryUseCase } from './update-category-usecase'
+import { CategoryNotFoundError } from '~/errors/category-not-found-error'
+import { error } from 'console'
+
+afterAll(async () => await TestHelpers.clearDatabase())
+
+describe('handle', () => {
+	it('should be able to update category', async () => {
+		const category: Category = {
+			title: 'New Category Test',
+			description: 'Category for test use case',
+		}
+
+		const newCategoryData: Category = {
+			title: 'Updated Category Test',
+			description: 'Updated category for test use case',
+		}
+
+		const newCategory = await CreateCategoryUseCase.handle(category)
+
+		if (newCategory.id === undefined) throw new Error('Category ID not found')
+
+		const { id, title, description } = await UpdateCategoryUseCase.handle(newCategory.id, newCategoryData)
+
+		expect(id).toEqual(newCategory.id)
+		expect(title).toEqual(newCategoryData.title)
+		expect(description).toEqual(newCategoryData.description)
+	})
+
+	it('it should not be possible to return the category if it does not exist', async () => {
+		const errorCategory: Category = {
+			title: 'Error category',
+			description: 'Error description',
+		}
+
+		await expect(UpdateCategoryUseCase.handle(0, errorCategory)).rejects.toBeInstanceOf(CategoryNotFoundError)
+	})
+})
---
+++ src/use-cases/categories/update-category-usecase.ts
@@ -0,0 +1,35 @@
+import { CategoryNotFoundError } from '~/errors/category-not-found-error'
+import { prisma } from '~/lib'
+import { Category } from '~/schemas/category'
+
+const handle = async (categoryId: number, category: Category): Promise<Category> => {
+	const categoryInDatabase = await prisma.category.findUnique({
+		where: {
+			id: categoryId,
+		},
+	})
+
+	if (categoryInDatabase === null) throw new CategoryNotFoundError(categoryId)
+
+	const { title, description } = category
+
+	const updatedCategory = await prisma.category.update({
+		where: {
+			id: categoryId,
+		},
+		data: {
+			title,
+			description,
+		},
+	})
+
+	return {
+		id: updatedCategory.id,
+		title: updatedCategory.title,
+		description: updatedCategory.description ?? undefined,
+	}
+}
+
+export const UpdateCategoryUseCase = {
+	handle,
+}
