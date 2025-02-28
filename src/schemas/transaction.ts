cdd01de6c0826d26f32d2a45aa87f38e3d17ac06
Lucas Pope <lpopedv@proton.me>
2025-02-28 18:58:32 -0300

    feat: move entity schemas to new directory

---
+++ src/schemas/transaction.ts
@@ -0,0 +1,16 @@
+import z from 'zod'
+import { categoryZodSchema } from './category'
+
+export const transactionZodSchema = z.object({
+	id: z.number().optional(),
+	categoryId: z.number(),
+	category: categoryZodSchema.optional(),
+	title: z.string(),
+	movement: z.enum(['income', 'outgoing']),
+	valueInCents: z.number(),
+	date: z.string().optional(),
+	isFixed: z.boolean().default(false),
+	isPaid: z.boolean().default(false),
+})
+
+export type Transaction = z.infer<typeof transactionZodSchema>
