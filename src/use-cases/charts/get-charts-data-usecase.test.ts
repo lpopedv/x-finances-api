d3f3d572a16d11062df870e520e89271921a5122
Lucas Pope <lpopedv@proton.me>
2025-03-09 11:19:47 -0300

    feat: create 'getChartsDataUseCase' test

---
+++ src/use-cases/charts/get-charts-data-usecase.test.ts
@@ -0,0 +1,57 @@
+import { afterAll, describe, it, expect } from 'vitest'
+import { TestHelpers } from '~/utils/test-helpers'
+import { CreateCategoryUseCase } from '../categories/create-category-usecase'
+import { CreateTransactionUseCase } from '../transactions/create-transaction-usecase'
+import { GetChartsDataUseCase } from './get-charts-data-usecase'
+
+afterAll(async () => await TestHelpers.clearDatabase())
+
+describe('handle', () => {
+	it('should be able to get spentsByCategory data', async () => {
+		const category01 = await CreateCategoryUseCase.handle({ title: 'Category 01' })
+		const category02 = await CreateCategoryUseCase.handle({ title: 'Category 02' })
+
+		const category01Transaction01 = await CreateTransactionUseCase.handle({
+			categoryId: category01.id as number,
+			title: 'transaction 01 category 01',
+			isFixed: false,
+			isPaid: false,
+			movement: 'outgoing',
+			valueInCents: 15000,
+			date: new Date().toISOString(),
+		})
+
+		const category01Transaction02 = await CreateTransactionUseCase.handle({
+			categoryId: category01.id as number,
+			title: 'transaction 02 category 01',
+			isFixed: false,
+			isPaid: false,
+			movement: 'outgoing',
+			valueInCents: 25000,
+			date: new Date().toISOString(),
+		})
+
+		const category02Transaction01 = await CreateTransactionUseCase.handle({
+			categoryId: category02.id as number,
+			title: 'transaction 01 category 02',
+			isFixed: true,
+			isPaid: false,
+			movement: 'outgoing',
+			valueInCents: 20000,
+			date: new Date().toISOString(),
+		})
+
+		const category01Total = category01Transaction01.valueInCents + category01Transaction02.valueInCents
+		const category02Total = category02Transaction01.valueInCents
+
+		const { spentsByCategory } = await GetChartsDataUseCase.handle()
+
+		console.log(spentsByCategory[0])
+
+		expect(spentsByCategory[0].category).toEqual(category01.title)
+		expect(spentsByCategory[0].spent).toEqual(category01Total)
+
+		expect(spentsByCategory[1].category).toEqual(category02.title)
+		expect(spentsByCategory[1].spent).toEqual(category02Total)
+	})
+})
