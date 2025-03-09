96d8b9e9c3b613f7e2e45d44022978ba6ed8fbb6
Lucas Pope <lpopedv@proton.me>
2025-03-09 11:04:44 -0300

    feat: create 'getChartsData' use case

---
+++ src/use-cases/charts/get-charts-data-usecase.ts
@@ -0,0 +1,48 @@
+import { startOfMonth, endOfMonth } from 'date-fns'
+import { prisma } from '~/database/prisma-client'
+
+const handle = async () => {
+	const spentsByCategory = await getSpentsByCategory()
+
+	return {
+		spentsByCategory,
+	}
+}
+
+const getSpentsByCategory = async () => {
+	const startDate = startOfMonth(new Date())
+	const endDate = endOfMonth(new Date())
+
+	const transactions = await prisma.transaction.groupBy({
+		by: ['categoryId'],
+		where: {
+			movement: 'outgoing',
+			date: {
+				gte: startDate,
+				lte: endDate,
+			},
+		},
+		_sum: {
+			valueInCents: true,
+		},
+		orderBy: {
+			_sum: { valueInCents: 'desc' },
+		},
+	})
+
+	const categories = await prisma.category.findMany({
+		where: { id: { in: transactions.map((transaction) => transaction.categoryId) } },
+		select: { id: true, title: true },
+	})
+
+	const chartData = transactions.map((transaction) => ({
+		category: categories.find((category) => category.id === transaction.categoryId)?.title || 'Desconhecido',
+		spent: (transaction._sum.valueInCents || 0) / 100,
+	}))
+
+	return chartData
+}
+
+export const GetChartsDataUseCase = {
+	handle,
+}
