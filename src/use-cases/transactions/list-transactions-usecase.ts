81101a7f1152fa60f355efc0608382e88c64ddc6
Lucas Pope <lpopedv@proton.me>
2025-03-11 20:44:32 -0300

    feat: include title in transactions query

--- src/use-cases/transactions/list-transactions-usecase.ts
+++ src/use-cases/transactions/list-transactions-usecase.ts
@@ -2,13 +2,20 @@ import { prisma } from '~/database/prisma-client'
 import type { Transaction } from '~/schemas/transaction'
 
 const handle = async (): Promise<Transaction[]> => {
-	const databaseTransactions = await prisma.transaction.findMany()
+	const databaseTransactions = await prisma.transaction.findMany({
+		include: {
+			category: true,
+		},
+	})
 
 	const parsedTransactions = databaseTransactions.map(
 		(transaction): Transaction => ({
 			id: transaction.id,
 			categoryId: transaction.categoryId,
 			title: transaction.title,
+			category: {
+				title: transaction.title,
+			},
 			movement: transaction.movement as 'income' | 'outgoing',
 			valueInCents: transaction.valueInCents,
 			date: transaction.date?.toISOString() ?? undefined,
